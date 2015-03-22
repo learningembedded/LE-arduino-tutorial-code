5fbb4f87ba7d9e64ffa02545ac62d86ebcd8f9c0
Learning  Embedded <contact@learningembedded.com>
2015-03-22 22:08:32 +0530

    Update A_4sevseg.ino

--- SEVENSEGMENT/A_4sevseg.ino
+++ SEVENSEGMENT/A_4sevseg.ino
@@ -15,18 +15,18 @@
 // http://learningembedded.com / 
 
 int count,i,j,k,l;
-int  arr[10]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x67}; # values for each digit stored in an array
+int  arr[10]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x67}; // values for each digit stored in an array
 
 void setup()
 {
     for( count= 0; count < 12; count ++ )
     {
-     pinMode(count, OUTPUT);                                      # initializing all pins till 11 as output
+     pinMode(count, OUTPUT);                                      // initializing all pins till 11 as output
     }
-     digitalWrite(11, HIGH);  # Switch off  7 segments connected to this pin as high is off and low is on
-     digitalWrite(10, HIGH);  # Switch off  7 segments connected to this pin as high is off and low is on
-     digitalWrite(9, HIGH);   # Switch off  7 segments connected to this pin as high is off and low is on
-     digitalWrite(8, HIGH);   # Switch off  7 segments connected to this pin as high is off and low is on
+     digitalWrite(11, HIGH);  // Switch off  7 segments connected to this pin as high is off and low is on
+     digitalWrite(10, HIGH);  // Switch off  7 segments connected to this pin as high is off and low is on
+     digitalWrite(9, HIGH);   // Switch off  7 segments connected to this pin as high is off and low is on
+     digitalWrite(8, HIGH);   // Switch off  7 segments connected to this pin as high is off and low is on
         
 }
 void loop()
@@ -42,26 +42,26 @@ void loop()
          digitalWrite(11, HIGH);
          digitalWrite(10, HIGH);
          digitalWrite(9, HIGH);
-         digitalWrite(8, LOW);                     # switch on segment 4
-         PORTD = arr[i];                           # put the value of array to the 7 segment using index as i
+         digitalWrite(8, LOW);                     // switch on segment 4
+         PORTD = arr[i];                           // put the value of array to the 7 segment using index as i
           delay(20);
          digitalWrite(11, HIGH);
          digitalWrite(10, HIGH);
          digitalWrite(8, HIGH);
-         digitalWrite(9, LOW);                    # switch on segment 3
-         PORTD = arr[j];                          # put the value of array to the 7 segment using index as j
+         digitalWrite(9, LOW);                    // switch on segment 3
+         PORTD = arr[j];                          // put the value of array to the 7 segment using index as j
           delay(20);
          digitalWrite(11, HIGH); 
          digitalWrite(8, HIGH);
          digitalWrite(9, HIGH);
-         digitalWrite(10, LOW);                   # switch on segment 2
-         PORTD = arr[k];                          # put the value of array to the 7 segment using index as k
+         digitalWrite(10, LOW);                   // switch on segment 2
+         PORTD = arr[k];                          // put the value of array to the 7 segment using index as k
           delay(20);
          digitalWrite(8, HIGH);
          digitalWrite(10, HIGH);
          digitalWrite(9, HIGH);
-         digitalWrite(11, LOW);                   # switch on segment 1
-         PORTD = arr[l];                          # put the value of array to the 7 segment using index as l
+         digitalWrite(11, LOW);                   // switch on segment 1
+         PORTD = arr[l];                          // put the value of array to the 7 segment using index as l
           delay(20);
         }
       }
